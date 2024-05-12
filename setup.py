f4325b2bcbdea3b1caa6eb56ff8f64b9f4c22d65
temporraryaccounthasbeensuspended <wweziza@icloud.com>
2024-05-13 01:39:14 +0700

    setup initialized

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sequentialdw',
     version='0.1',
-    packages=['sequentialdw'],
-    install_requires=[
-        'requests',
-        'python-dotenv',
-    ],
+    packages=find_packages(),
+    install_requires=['requests'],
+    entry_points={
+        'console_scripts': [
+            'seqdownload = sequentialdw.seq_download:main'  
+        ]
+    },
+    author='wweziza',
+    description='A simple website scrapper with sequential files in it.'
 )
