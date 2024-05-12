9931c3abb810d8b436492ab6cc788a32e5b2ce25
temporraryaccounthasbeensuspended <wweziza@icloud.com>
2024-05-13 01:29:02 +0700

    First push

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sequentialdw',
+    version='0.1',
+    packages=['sequentialdw'],
+    install_requires=[
+        'requests',
+        'python-dotenv',
+    ],
+)
